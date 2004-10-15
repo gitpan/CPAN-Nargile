6544953bdcc34f95314f8d7d9fa2962ff65df889
גאבור סבו - Gábor Szabó <gabor@pti.co.il>
2004-10-15 01:54:10 +0000

    Import of SZABGAB/CPAN-Nargile-0.02 from CPAN.
    gitpan-cpan-distribution: CPAN-Nargile
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SZABGAB/CPAN-Nargile-0.02.tar.gz
    gitpan-cpan-author:       SZABGAB
    gitpan-cpan-maturity:     released

---
+++ lib/CPAN/Nargile.pm
@@ -0,0 +1,26 @@
+package CPAN::Nargile;
+
+our $VERSION = "0.02";
+
+
+1;
+
+=head1 NAME
+
+nargile - CPAN smoking that makes you dizzy
+
+
+See documentation in the nargile.pl script
+
+This is an experimental version that works on my computer...
+
+
+
+=head1 Spelling
+
+ Maybe it was better if I spelled it as Nargila ?
+
+
+
+=cut
+
